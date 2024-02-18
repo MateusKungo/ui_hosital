7ca0b431372b4610a07b8199f9d23c27f063a980
Mateus Drake <108342680+MateusKungo@users.noreply.github.com>
2024-02-18 14:02:31 +0100

    comple

---
+++ admin/modalSalvo.js
@@ -0,0 +1,9 @@
+const modal = document.querySelector('.modal-container')
+
+function openModalSalvo() {
+  modal.classList.add('active')
+}
+
+function closeModalSalvo() {
+  modal.classList.remove('active')
+}
